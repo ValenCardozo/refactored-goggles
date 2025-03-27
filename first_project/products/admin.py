3bd1ded6bb27292e354f604ce6ba83bbd265667d
ValenCardozo <valencardozo7@gmail.com>
2025-03-26 22:44:49 -0300

    feat(admin): add models

--- first_project/products/admin.py
+++ first_project/products/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
-from products.models import Product
+from products.models import Customer, Order, OrderDetail, Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price')
     list_filter = ('name', 'price')
     search_fields = ('name', 'description')
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'phone')
+    list_filter = ('name', 'email', 'phone')
+    search_fields = ('name', 'email')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'date')
+
+@admin.register(OrderDetail)
+class OrderDetailAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'product', 'quantity')
+    list_filter = ('order', 'quantity')
+    search_fields = ('order', 'product')
